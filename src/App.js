8007a96623160321e12cb50aaab060580f23b804
shebinkkabeer <shebinkkabeer@gmail.com>
2020-06-01 20:01:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
+
+import { SocialIcon } from 'react-social-icons';
 import styles from './App.module.css';
 
 import image from './images/image.png';
@@ -31,9 +33,42 @@ class App extends React.Component {
       <div className={styles.container}>
         <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
+
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} /> 
+        <footer className="footer bg-dark mt-auto py-3">
+        <div className="container-fluid bg-success text-white text-center py-3">
+          <br></br>
+          <br></br>
+          <br></br>
+
+
+
+          <div className="row>">
+          <SocialIcon url="http://instagram.com/shebin_dilz" network="instagram" />
+          &nbsp;
+          &nbsp;
+
+          <SocialIcon url="http://github.com/shebinkkabeer" network="github" />
+          &nbsp;
+          &nbsp;
+
+
+          <SocialIcon url="mailto:shebinkkabeer@gmail.com" network="google" />
+
+
+
+          </div>
+        </div>
+        <div className="container">
+          <span className="text-muted">
+            Developed by @<span className="text-white">shebin_dilz</span> 
+          </span>
+        </div>
+      </footer>
+
       </div>
+
     );
   }
 }
