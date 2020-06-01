34e843e985d7478ef0fc47770b70789994107399
shebinkkabeer <shebinkkabeer@gmail.com>
2020-06-01 20:44:28 +0530

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -19,7 +19,7 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
               Infected
             </Typography>
             <Typography variant="h5" component="h2">
-              <CountUp start={0} end={confirmed.value} duration={2.75} separator="," />
+              <CountUp start={0} end={confirmed.value} duration={2} separator="," />
             </Typography>
             <Typography color="textSecondary">
               {new Date(lastUpdate).toDateString()}
